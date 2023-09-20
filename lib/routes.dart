0cf3f40b61bd511cc0a9d1a5ef7ba9f35a31d02c
JuanOnate <19030179@itcelaya.edu.mx>
2023-09-20 14:22:41 -0600

    Add Task
    Agregar tarea

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/widgets.dart';
+import 'package:pmsn20232/screens/add_task.dart';
 import 'package:pmsn20232/screens/dashboard_screen.dart';
 import 'package:pmsn20232/screens/tarea_screen.dart';
 
 Map<String, WidgetBuilder> getRoutes(){
   return{
     '/dash' : (BuildContext context) => DashboardScreen(),
-    '/tarea' : (BuildContext context) => TareaScreen()
+    '/tarea' : (BuildContext context) => TareaScreen(),
+    '/add' : (BuildContext context) => AddTask(),
   };
 }
